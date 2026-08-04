15f75b9e3fd8d9a7b9ed0b8cdce689c5cdf78fab
Carrie Holt <cholt@lco.global>
2026-08-04 16:19:08 -0700

    added photometry functions

---
+++ src/lsst_aot/photometry.py
@@ -0,0 +1,23 @@
+"""
+Photometry functions for measuring flux from model comet images.
+"""
+import numpy as np
+
+from .models import make_nucleus_psf
+from .utils import LSST_PIXEL_SCALE, rho_grid, seeing_to_sigma_pixels
+
+
+def psf_photometry(image, center, sigma=seeing_to_sigma_pixels(0.75)):
+    """
+    Estimate flux by fitting a point source with the given PSF sigma (the
+    same PSF as field stars) via a weighted least-squares fit.
+    """
+    template = make_nucleus_psf(image.shape, center, flux=1.0, sigma=sigma)
+    return np.sum(image * template) / np.sum(template ** 2)
+
+
+def aperture_photometry(image, center, radius_arcsec=2.4, pixel_scale=LSST_PIXEL_SCALE):
+    """Sum flux within a circular aperture of the given radius (arcsec)."""
+    radius_px = radius_arcsec / pixel_scale
+    rho = rho_grid(image.shape, center)
+    return image[rho <= radius_px].sum()
